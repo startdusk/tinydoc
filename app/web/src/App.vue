b2074194e85be6456908ffc8856ee694a4758043
startdusk <522383345@qq.com>
2022-07-10 22:03:18 +0800

    feat✨:  extract component for vditor

--- app/web/src/App.vue
+++ app/web/src/App.vue
@@ -1,68 +1,15 @@
 <template>
-  <div id="vditor" />
+  <Vditor :markdown="markdown" />
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import Vditor from "vditor";
-import $ from "jquery";
-import "vditor/dist/index.css";
+import Vditor from "./components/Vditor.vue";
+const markdown = `
+        ## Vue Composition API + Vditor + TypeScript Minimal Example
 
-const vditor = ref<Vditor | null>(null);
+        Topic 1
 
-onMounted(async () => {
-  vditor.value = new Vditor("vditor", {
-    height: window.innerHeight,
-    cache: { enable: false },
-    value: "",
-    toolbar: [
-      {
-        name: "more",
-        toolbar: [
-          "code-theme",
-          "content-theme",
-          "export",
-          "outline",
-          "preview",
-        ],
-      },
-    ],
-    toolbarConfig: {
-      hide: true,
-    },
-    outline: {
-      enable: true,
-      position: "left",
-    },
-    preview: {
-      mode: "editor",
-      actions: [],
-      hljs: {
-        enable: true,
-        lineNumber: true,
-      },
-    },
-    after: async () => {
-      // vditor.value is a instance of Vditor now and thus can be safely used here
-      vditor.value!.setValue(
-        `## Vue Composition API + Vditor + TypeScript Minimal Example
-        
-        ### Topic 1
-
-        #### Topic 2
-        `
-      );
-      const previewBtn = $("button[data-type='preview']");
-      console.log(previewBtn);
-      previewBtn.click();
-    },
-  });
-});
+        Topic 2
+        `;
 </script>
-<style scoped>
-:global(.vditor-toolbar--hide) {
-  transition: all 0.15s ease-in-out;
-  height: 0px !important;
-  overflow: hidden;
-}
-</style>
+<style scoped></style>
---
+++ app/web/src/components/Vditor.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="vditor" />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import Vditor from "vditor";
+import $ from "jquery";
+import "vditor/dist/index.css";
+
+interface Props {
+  markdown: string;
+}
+
+const { markdown } = defineProps<Props>();
+
+const vditor = ref<Vditor | null>(null);
+
+onMounted(async () => {
+  vditor.value = new Vditor("vditor", {
+    height: window.innerHeight,
+    cache: { enable: false },
+    value: "",
+    toolbar: [
+      {
+        name: "more",
+        toolbar: [
+          "code-theme",
+          "content-theme",
+          "export",
+          "outline",
+          "preview",
+        ],
+      },
+    ],
+    toolbarConfig: {
+      hide: true,
+    },
+    outline: {
+      enable: true,
+      position: "left",
+    },
+    preview: {
+      mode: "editor",
+      actions: [],
+      hljs: {
+        enable: true,
+        lineNumber: true,
+      },
+    },
+    after: async () => {
+      // vditor.value is a instance of Vditor now and thus can be safely used here
+      vditor.value!.setValue(markdown);
+      const previewBtn = $("button[data-type='preview']");
+      previewBtn.trigger("click");
+    },
+  });
+});
+</script>
+<style scoped>
+:global(.vditor-toolbar--hide) {
+  transition: all 0.15s ease-in-out;
+  height: 0px !important;
+  overflow: hidden;
+}
+</style>
